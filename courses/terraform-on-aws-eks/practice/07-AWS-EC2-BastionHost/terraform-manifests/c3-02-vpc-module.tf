02bdebec52edf03a311a34b043843ba8fe99e17e
Typhoon <tayfunaliosman@gmail.com>
2025-10-18 18:04:19 +0300

    fixes to code so it can run

--- courses/terraform-on-aws-eks/practice/07-AWS-EC2-BastionHost/terraform-manifests/c3-02-vpc-module.tf
+++ courses/terraform-on-aws-eks/practice/07-AWS-EC2-BastionHost/terraform-manifests/c3-02-vpc-module.tf
@@ -9,15 +9,16 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   #version = "3.11.0"
   #version = "~> 3.11"
-  version = "4.0.1"  
-  
+  # version = "4.0.1"
+  version = "5.4.0"
+
   # VPC Basic Details
   name = local.eks_cluster_name
   cidr = var.vpc_cidr_block
   #azs             = var.vpc_availability_zones
   azs             = data.aws_availability_zones.available.names
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets  
+  private_subnets = var.vpc_private_subnets
 
   # Database Subnets
   database_subnets = var.vpc_database_subnets
@@ -25,29 +26,29 @@ module "vpc" {
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   # create_database_internet_gateway_route = true
   # create_database_nat_gateway_route = true
-  
+
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.vpc_enable_nat_gateway 
+  enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
 
   # VPC DNS Parameters
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  
+
   tags = local.common_tags
   vpc_tags = local.common_tags
 
   # Additional Tags to Subnets
   public_subnet_tags = {
     Type = "Public Subnets"
-    "kubernetes.io/role/elb" = 1    
-    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"        
+    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
   }
   private_subnet_tags = {
     Type = "private-subnets"
-    "kubernetes.io/role/internal-elb" = 1    
-    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"
   }
 
   database_subnet_tags = {
--- courses/terraform-on-aws-eks/practice/07-AWS-EC2-BastionHost/terraform-manifests/c4-06-ec2bastion-elasticip.tf
+++ courses/terraform-on-aws-eks/practice/07-AWS-EC2-BastionHost/terraform-manifests/c4-06-ec2bastion-elasticip.tf
@@ -3,6 +3,6 @@
 resource "aws_eip" "bastion_eip" {
   depends_on = [module.ec2_public, module.vpc ]
   instance = module.ec2_public.id
-  vpc      = true
+  # vpc      = true
   tags = local.common_tags  
 }
