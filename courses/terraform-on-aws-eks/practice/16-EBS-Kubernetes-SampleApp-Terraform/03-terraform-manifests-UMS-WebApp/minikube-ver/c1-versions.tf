bf84dfc2bb4783706f6c9b7fc33625645b80d0d5
Typhoon <tayfunaliosman@gmail.com>
2025-10-24 08:42:39 +0300

    16 - 03 - minikube ver

--- courses/terraform-on-aws-eks/practice/16-EBS-Kubernetes-SampleApp-Terraform/03-terraform-manifests-UMS-WebApp/minikube-ver/c1-versions.tf
+++ courses/terraform-on-aws-eks/practice/16-EBS-Kubernetes-SampleApp-Terraform/03-terraform-manifests-UMS-WebApp/minikube-ver/c1-versions.tf
@@ -2,11 +2,6 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      #version = ">= 4.65"
-      version = ">= 5.31"
-     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       #version = "~> 2.7.1"
@@ -14,13 +9,5 @@ terraform {
     }     
   }
   # Adding Backend as S3 for Remote State Storage
-  backend "s3" {
-    bucket = "terraform-on-aws-eks"
-    key    = "dev/ebs-sampleapp-demo/terraform.tfstate"
-    region = "us-east-1" 
-
-    # For State Locking
-    dynamodb_table = "dev-ebs-sampleapp-demo"    
-  }    
 }
 
--- courses/terraform-on-aws-eks/practice/16-EBS-Kubernetes-SampleApp-Terraform/03-terraform-manifests-UMS-WebApp/minikube-ver/c4-02-persistent-volume-claim.tf
+++ courses/terraform-on-aws-eks/practice/16-EBS-Kubernetes-SampleApp-Terraform/03-terraform-manifests-UMS-WebApp/minikube-ver/c4-02-persistent-volume-claim.tf
@@ -5,7 +5,7 @@ resource "kubernetes_persistent_volume_claim_v1" "pvc" {
   }
   spec {
     access_modes = ["ReadWriteOnce"]
-    storage_class_name = kubernetes_storage_class_v1.ebs_sc.metadata.0.name 
+    storage_class_name = "standard"
     resources {
       requests = {
         storage = "4Gi"
