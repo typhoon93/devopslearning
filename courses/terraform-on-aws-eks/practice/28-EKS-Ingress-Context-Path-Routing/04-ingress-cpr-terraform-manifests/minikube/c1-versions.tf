03a3ddbcb0678584c207830179ddf95efa02c3db
Typhoon <tayfunaliosman@gmail.com>
2025-10-31 20:20:59 +0200

    28 - 04 - minikube

---
+++ courses/terraform-on-aws-eks/practice/28-EKS-Ingress-Context-Path-Routing/04-ingress-cpr-terraform-manifests/minikube/c1-versions.tf
@@ -0,0 +1,11 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      #version = "~> 2.7"
+      version = ">= 2.20"
+    }    
+  }
+}
---
+++ courses/terraform-on-aws-eks/practice/28-EKS-Ingress-Context-Path-Routing/04-ingress-cpr-terraform-manifests/minikube/c3-providers.tf
@@ -0,0 +1,6 @@
+
+# Terraform Kubernetes Provider
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  config_context = "minikube"
+}
